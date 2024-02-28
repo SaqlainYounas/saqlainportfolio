2b2fa2595918fdd12b0fa0f6428ac84b772a1b25
Muhammad Saqlain Younas <saqlain.younas@systemsltd.com>
2024-02-28 13:00:53 +0500

    fix 1

--- src/actions/getProjects.ts
+++ src/actions/getProjects.ts
@@ -1,10 +1,10 @@
 import {projectsData} from "@/lib/data";
 import axios from "axios";
-import {Project} from "../../types";
+import {ProjectType} from "../../types";
 
 export default async function getProjects() {
   try {
-    let response: Project[] = projectsData;
+    let response: ProjectType[] = projectsData;
     return response;
   } catch (error) {
     throw new Error("Failed to Fetch Data");
