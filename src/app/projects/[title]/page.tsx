b60fa75f1f3421c6960d79b35660b55b215507d9
Muhammad Saqlain Younas <saqlain.younas@systemsltd.com>
2024-02-28 12:49:42 +0500

    Build Failure Fix

--- src/app/projects/[title]/page.tsx
+++ src/app/projects/[title]/page.tsx
@@ -5,22 +5,9 @@ import SectionHeading from "@/components/section-heading";
 import {motion} from "framer-motion";
 import axios from "axios";
 import {projectsData} from "@/lib/data";
+import {fadeInAnimationVariants} from "@/lib/types";
 
 type ProjectProps = (typeof projectsData)[number];
-
-export const fadeInAnimationVariants = {
-  initial: {
-    opacity: 0,
-    y: 100,
-  },
-  animate: (index: number) => ({
-    opacity: 1,
-    y: 0,
-    transition: {
-      delay: 0.05 * index,
-    },
-  }),
-};
 
 export default function IndividualProject({params}: {params: {title: string}}) {
   const [CurrentProject, setCurrentProject] = useState<ProjectProps>();
